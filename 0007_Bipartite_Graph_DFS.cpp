16f07e61dada6a870ec81e5df5197fd5ec4f5ffe
VICKY <vishujoshi21nov2000@gmail.com>
2022-05-21 19:45:49 +0530

    Biparite Graph DFS

---
+++ 0007_Bipartite_Graph_DFS.cpp
@@ -0,0 +1,70 @@
+// Bipatite Graph (DFS)
+
+// A graph that can be colored using 2 colors such that no two adjacent nodes have same color.
+// Odd length cycle than graph is not bipatite.
+// If it doesn't have a odd length cycle it is a bipatite grpah.
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool dfsCheck(int sv, vector<int> adj[], vector<int> &color)
+{
+    if (color[sv] == -1)
+        color[sv] = 1;
+
+    for (auto itr : adj[sv])
+    {
+        if (color[itr] == -1)
+        {
+            color[itr] = 1 - color[sv];
+
+            if (!dfsCheck(itr, adj, color))
+                return false;
+        }
+        else if (color[itr] == color[sv])
+            return false;
+    }
+    return true;
+}
+
+bool CheckBipartite(vector<int> adj[], int n)
+{
+    vector<int> color(n, -1);
+
+    for (int i = 0; i < n; ++i)
+    {
+        if (color[i] == -1)
+        {
+            if (!dfsCheck(i, adj, color))
+                return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    int n, m;
+    cout << "Enter Number of Nodes and Edges : ";
+    cin >> n >> m;
+
+    vector<int> adj[n + 1];
+
+    for (int i = 0; i < m; ++i)
+    {
+        int u, v;
+        cout << "Enter Vertices of the Graph : ";
+        cin >> u >> v;
+
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    if (CheckBipartite(adj, n))
+        cout << "Yes, the given Graph is Bipartite" << endl;
+    else
+        cout << "No, the given Graph is not Bipartite" << endl;
+
+    return 0;
+}
